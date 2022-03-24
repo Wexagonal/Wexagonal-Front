5de9dfd15781d1625db153ef186e650bba7a5028
ChenYFan <chen2778754364@foxmail.com>
2022-03-24 14:05:19 +0000

    A 添加列表功能

---
+++ GList.js
@@ -0,0 +1,28 @@
+//NodeJS 调用fs深搜列出当前文件夹下所有文件,并以数组存储为list.json
+const fs = require('fs');
+const res = []
+const ignore = [
+    'node_modules',
+    '.git',
+    'package.json',
+    '.gitattributes',
+    'CNAME'
+]
+const list = (path) => {
+    const dir = fs.readdirSync(path);
+    dir.forEach(file => {
+        const stat = fs.statSync(path + '/' + file);
+        if (ignore.indexOf(file) === -1) {
+            if (stat.isDirectory()) {
+                return list(path + '/' + file);
+            } else {
+                res.push(path + '/' + file);
+            }
+        }
+    })
+}
+list('.')
+for(let i = 0; i < res.length; i++){
+    res[i] = res[i].replace('./', 'https://SOURCE_CDN/');
+}
+fs.writeFileSync('list.json', JSON.stringify(res));
